13527907348b9ccc03edfb2bd911dc84ae39af00
Paul May <git@paulmay.org>
2013-01-19 16:05:50 -0500

    depolying methods automagically

---
+++ _site/deploy.sh
@@ -0,0 +1,9 @@
+cd ~/Dropbox/Sites/methods.paulmay.org
+rm -rf _site
+jekyll
+git add _site/* -f
+git add _posts/* -f
+git add _drafts/* -f
+git commit -a -m "depolying methods automagically"
+git push heroku master
+git push origin master
